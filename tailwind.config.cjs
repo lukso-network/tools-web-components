b0fd7035a88d23d5a9eb3deee19854a92b7ada0d
Andreas Richter <708186+richtera@users.noreply.github.com>
2023-01-10 07:49:28 -0500

    fix: Husky

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,10 @@ module.exports = {
     extend: {
       colors: {
         neutral: {
-          20: "#243542",
-          25: "#2D4353",
-          90: "#DEE7ED",
-          100: "#FFFFFF",
+          20: '#243542',
+          25: '#2D4353',
+          90: '#DEE7ED',
+          100: '#FFFFFF',
         },
       },
     },

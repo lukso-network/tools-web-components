02ffaed24fb0ada3b3f0c0e651cd8038fbcb4889
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-01-13 16:23:56 +0100

    Reorganize files

--- src/shared/tailwind.element.ts
+++ src/shared/tailwind.element.ts
@@ -1,6 +1,6 @@
 import { LitElement, unsafeCSS } from 'lit'
 
-import style from './tailwind.global.scss?inline'
+import style from './styles/component.scss?inline'
 
 const tailwindElement = unsafeCSS(style)
 

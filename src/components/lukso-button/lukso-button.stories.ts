7c8306961cc6527bdc1630c70359e4f91b5758db
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-01-18 14:38:35 +0100

    Add landing variant

--- src/components/lukso-button/lukso-button.stories.ts
+++ src/components/lukso-button/lukso-button.stories.ts
@@ -9,7 +9,7 @@ export default {
   argTypes: {
     variant: {
       control: { type: 'select' },
-      options: ['primary', 'secondary'],
+      options: ['primary', 'secondary', 'landing'],
     },
     disabled: {
       control: { type: 'boolean' },
@@ -37,17 +37,7 @@ Primary.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement)
 
   // 👇 Assert DOM structure
-  await expect(canvas.getByText('Hello World')).toBeInTheDocument()
-
-  // See https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args to learn how to setup logging in the Actions panel
-  // await userEvent.click(canvas.getByTestId("button"));
-
-  // 👇 Assert DOM structure
-  // await expect(
-  //   canvas.getByText(
-  //     "Hello World"
-  //   )
-  // ).toBeInTheDocument();
+  expect(canvas.getByText('Hello World')).toBeInTheDocument()
 }
 
 export const Secondary = Template.bind({})
@@ -55,54 +45,7 @@ Secondary.args = {
   variant: 'secondary',
 }
 
-// import { html } from 'lit-html'
-// import { Meta, Story } from '@storybook/react'
-// import './index'
-
-// const meta: Meta = {
-//   title: 'Web Components/Button',
-//   component: 'lukso-button',
-//   argTypes: {
-//     variant: {
-//       options: ['primary', 'secondary'],
-//       control: { type: 'select' },
-//     },
-//     disabled: {
-//       options: [true, false],
-//     },
-//     // children: {
-//     //   defaultValue: 'Click me!',
-//     //   control: {
-//     //     type: 'text',
-//     //   },
-//     // },
-//     // onClick: {
-//     //   action: 'clicked',
-//     // },
-//   },
-//   parameters: {
-//     controls: { expanded: true },
-//     docs: {
-//       page: null,
-//     },
-//     design: {
-//       type: 'figma',
-//       url: 'https://www.figma.com/file/NFCh20xAq3Jg2g8A0DNC9I/UI-Library?node-id=6%3A1324&t=eru1dzxSBVeJ3NJw-4',
-//     },
-//   },
-// }
-
-// export default meta
-
-// export function Primary({ variant, disabled }) {
-//   return html`<lukso-button variant=${variant} ?disabled=${disabled}
-//     >Hello World</lukso-button
-//   >`
-// }
-
-// // Secondary.args = {
-// //   variant: 'secondary',
-// //   disabled: false,
-// // }
-
-// Primary.args = { variant: 'primary', disabled: false }
+export const Landing = Template.bind({})
+Landing.args = {
+  variant: 'landing',
+}
--- src/components/lukso-button/lukso-button.ts
+++ src/components/lukso-button/lukso-button.ts
@@ -4,10 +4,12 @@ import { customElement, property } from 'lit/decorators.js'
 import { TailwindElement } from '../../shared/tailwind.element'
 import { customClassMap } from '../../shared/directives/custom-class-map'
 
+export type ButtonVariants = 'primary' | 'secondary' | 'landing'
+
 @customElement('lukso-button')
 export class LuksoButton extends TailwindElement {
   @property({ type: String })
-  variant = 'primary'
+  variant: ButtonVariants = 'primary'
 
   @property({ type: Boolean })
   disabled = false
@@ -18,8 +20,15 @@ export class LuksoButton extends TailwindElement {
   private secondaryStyles = `bg-neutral-100 border-neutral-90 text-neutral-20 disabled:bg-neutral-90
     disabled:border-neutral-90 disabled:text-neutral-100 hover:shadow-button-hover-secondary`
 
-  private primaryStyles = `bg-neutral-20 border-neutral-20 text-neutral-100 disabled:bg-neutral-90
-    disabled:border-neutral-90 hover:shadow-button-hover-primary`
+  private primaryStyles = `bg-neutral-20 border-neutral-20 text-neutral-100
+    disabled:bg-neutral-90
+    disabled:border-neutral-90
+    hover:shadow-button-hover-primary
+  `
+
+  private landingStyles = `bg-purple-51 border-purple-51 text-neutral-100
+    disabled:bg-neutral-90 disabled:border-neutral-90
+    hover:shadow-button-hover-primary hover:bg-purple-58 hover:border-purple-58`
 
   render() {
     return html`
@@ -30,6 +39,7 @@ export class LuksoButton extends TailwindElement {
           [this.defaultStyles]: true,
           [this.primaryStyles]: this.variant === 'primary',
           [this.secondaryStyles]: this.variant === 'secondary',
+          [this.landingStyles]: this.variant === 'landing',
         })}
       >
         <slot></slot>

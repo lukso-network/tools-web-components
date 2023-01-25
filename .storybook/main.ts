6a1753db717d6ca51ff2f2bb4e38f4848814ccc0
Andreas Richter <708186+richtera@users.noreply.github.com>
2023-01-25 12:13:38 -0500

    fix: Change vite (split config), storybook (postcss, scss)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,11 +3,14 @@ const path = require('path')
 
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  staticDirs: ['../src/shared/assets'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-controls',
+    '@storybook/preset-scss',
+    '@storybook/addon-postcss',
   ],
   framework: {
     name: '@storybook/web-components-vite',

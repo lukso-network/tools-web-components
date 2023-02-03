77f4fb5dfd1b6dd4961d1928cb2a7994e4def0af
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-02-03 12:34:35 +0100

    feat: tag component (#30)

---
+++ src/components/lukso-tag/index.ts
@@ -0,0 +1,68 @@
+import { html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+import { styleMap } from 'lit/directives/style-map.js'
+
+import { TailwindElement } from '@/shared/tailwind-element'
+import { customClassMap } from '@/shared/directives'
+
+export type TagSizes = 'small' | 'large'
+
+@customElement('lukso-tag')
+export class LuksoTag extends TailwindElement {
+  @property({ type: String })
+  size: TagSizes = 'small'
+
+  @property({ type: Boolean, attribute: 'is-rounded' })
+  isRounded = false
+
+  @property({ type: String, attribute: 'background-color' })
+  backgroundColor = ''
+
+  @property({ type: String, attribute: 'text-color' })
+  textColor = ''
+
+  private defaultStyles = `rounded-lg inline-flex items-center justify-center border border-neutral-20 text-neutral-20`
+  private smallStyles = `paragraph-12-medium h-[28px]`
+  private largeStyles = `paragraph-14-medium h-[34px]`
+  private roundedStyles = `rounded-[56px]`
+
+  private padding = () => {
+    if (this.size === 'small' && this.isRounded) {
+      return 'px-3'
+    }
+
+    if (this.size === 'large') {
+      return 'px-4'
+    }
+
+    return 'px-2'
+  }
+
+  render() {
+    return html`
+      <div
+        data-testid="tag"
+        class=${customClassMap({
+          [this.defaultStyles]: true,
+          [this.padding()]: true,
+          [this.roundedStyles]: this.isRounded,
+          [this.smallStyles]: this.size === 'small',
+          [this.largeStyles]: this.size === 'large',
+        })}
+        style=${styleMap({
+          backgroundColor: this.backgroundColor,
+          borderColor: this.backgroundColor,
+          color: this.textColor,
+        })}
+      >
+        <slot></slot>
+      </div>
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'lukso-tag': LuksoTag
+  }
+}
---
+++ src/components/lukso-tag/lukso-tag.stories.ts
@@ -0,0 +1,107 @@
+import { html } from 'lit-html'
+import './index'
+
+export default {
+  title: 'Design System/Components/Tag',
+  component: 'lukso-tag',
+  argTypes: {
+    content: {
+      control: { type: 'text' },
+    },
+    isRounded: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    size: {
+      control: {
+        type: 'select',
+      },
+      options: ['small', 'large'],
+    },
+    backgroundColor: {
+      control: {
+        type: 'text',
+      },
+    },
+    textColor: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+  args: {
+    size: 'small',
+    isRounded: false,
+    content: 'Small Tag',
+    backgroundColor: '',
+    textColor: '',
+  },
+  parameters: {
+    controls: {
+      exclude: [
+        'defaultStyles',
+        'smallStyles',
+        'largeStyles',
+        'roundedStyles',
+        'is-rounded',
+        'background-color',
+        'text-color',
+        'padding',
+      ],
+    },
+  },
+}
+
+const Template = ({ content, size, isRounded, backgroundColor, textColor }) =>
+  html`<lukso-tag
+    size=${size}
+    ?is-rounded=${isRounded}
+    background-color=${backgroundColor}
+    text-color=${textColor}
+    >${content}</lukso-tag
+  >`
+
+export const DefaultTag = Template.bind({})
+DefaultTag.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/NFCh20xAq3Jg2g8A0DNC9I/UI-Library?node-id=375%3A3234&t=AGmdbG8fXRENuU3o-4',
+  },
+}
+
+export const LargeTag = Template.bind({})
+LargeTag.args = {
+  size: 'large',
+  content: 'Large Tag',
+}
+LargeTag.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/NFCh20xAq3Jg2g8A0DNC9I/UI-Library?node-id=375%3A3234&t=AGmdbG8fXRENuU3o-4',
+  },
+}
+
+export const RoundedTag = Template.bind({})
+RoundedTag.args = {
+  isRounded: true,
+}
+RoundedTag.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/NFCh20xAq3Jg2g8A0DNC9I/UI-Library?node-id=1083%3A13483&t=AGmdbG8fXRENuU3o-4',
+  },
+}
+
+export const CustomColorTag = Template.bind({})
+CustomColorTag.args = {
+  content: 'Custom Color Tag',
+  backgroundColor: '#E96464',
+  textColor: '#FFFFFF',
+}
+CustomColorTag.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/NFCh20xAq3Jg2g8A0DNC9I/UI-Library?node-id=375%3A3241&t=AGmdbG8fXRENuU3o-4',
+  },
+}

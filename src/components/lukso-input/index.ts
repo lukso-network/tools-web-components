457c4a3c184ecd617977d131e15ac1c0f61caf0e
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-02-09 17:12:39 +0100

    Input component

---
+++ src/components/lukso-input/index.ts
@@ -0,0 +1,145 @@
+import { html, nothing } from 'lit'
+import { customElement, property, state } from 'lit/decorators.js'
+import { HTMLInputTypeAttribute } from 'react'
+
+import { TailwindElement } from '@/shared/tailwind-element'
+import { customClassMap } from '@/shared/directives'
+
+@customElement('lukso-input')
+export class LuksoInput extends TailwindElement {
+  @property({ type: String })
+  value = ''
+
+  @property({ type: String })
+  name = ''
+
+  @property({ type: String })
+  type: HTMLInputTypeAttribute = 'text'
+
+  @property({ type: String })
+  placeholder = ''
+
+  @property({ type: String })
+  label = ''
+
+  @property({ type: String })
+  description = ''
+
+  @property({ type: String })
+  error = ''
+
+  @property({ type: String })
+  unit = ''
+
+  @property({ type: Boolean, attribute: 'is-full-width' })
+  isFullWidth = false
+
+  @state()
+  private hasHocus = false
+
+  private defaultStyles = `bg-neutral-100 text-neutral-20 paragraph-16-regular px-4 py-3
+    border border-solid border-neutral-90 h-[48px] placeholder:neutral-70
+    outline-none transition transition-all duration-250`
+
+  private errorStyles = `border-red-85 text-red-65 focus:border-red-65`
+
+  private unitStyles = `paragraph-12-regular text-neutral-60 flex px-3.5 items-center relative
+    border border-solid border-neutral-90 h-[48px] transition transition-all duration-250
+    rounded-r-xl border-l-0 before:bg-neutral-90 before:absolute before:top-[calc(50%-12px)] before:left-0
+    before:w-[1px] before:h-[24px]`
+
+  inputTemplate() {
+    return html`
+      <input
+        data-testid="input"
+        id=${this.name}
+        type=${this.type}
+        value=${this.value}
+        placeholder=${this.placeholder}
+        class=${customClassMap({
+          [this.defaultStyles]: true,
+          [this.errorStyles]: this.error !== '',
+          ['border-neutral-35']: this.hasHocus,
+          ['rounded-l-xl border-r-0']: this.unit !== '',
+          ['rounded-xl']: this.unit === '',
+          ['w-full']: this.isFullWidth,
+        })}
+        @focus=${this.handleFocus}
+        @blur=${this.handleBlur}
+        @keyup=${this.handleKeyUp}
+      />
+    `
+  }
+
+  labelTemplate() {
+    return html`
+      <label for=${this.name} class="heading-h5 text-neutral-20 pb-2 block"
+        >${this.label}</label
+      >
+    `
+  }
+
+  descriptionTemplate() {
+    return html`
+      <div class="paragraph-12-regular text-neutral-20 pb-2">
+        ${this.description ?? nothing}
+      </div>
+    `
+  }
+
+  errorTemplate() {
+    return html`<div class="paragraph-12-regular text-red-65 pt-2">
+      ${this.error}
+    </div>`
+  }
+
+  unitTemplate() {
+    return html`<div
+      class=${customClassMap({
+        [this.unitStyles]: true,
+        ['border-neutral-35']: this.hasHocus,
+      })}
+    >
+      ${this.unit}
+    </div>`
+  }
+
+  private handleFocus() {
+    this.hasHocus = true
+  }
+
+  private handleBlur() {
+    this.hasHocus = false
+  }
+
+  private handleKeyUp(event: KeyboardEvent) {
+    const target = event.target as HTMLInputElement
+    const keyUpEvent = new CustomEvent('on-key-up', {
+      detail: {
+        value: target.value,
+      },
+      bubbles: true,
+      composed: true,
+    })
+    this.dispatchEvent(keyUpEvent)
+  }
+
+  render() {
+    return html`
+      <div>
+        ${this.label ? this.labelTemplate() : nothing}
+        ${this.description ? this.descriptionTemplate() : nothing}
+        <div class="flex">
+          ${this.inputTemplate()} ${this.unit ? this.unitTemplate() : nothing}
+        </div>
+        ${this.error ? this.errorTemplate() : nothing}
+      </div>
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'lukso-input': LuksoInput
+  }
+}
---
+++ src/components/lukso-input/lukso-input.stories.ts
@@ -0,0 +1,138 @@
+import { html } from 'lit-html'
+import './index'
+
+/**  Documentation and examples of `lukso-input` component.  */
+export default {
+  title: 'Design System/Forms/lukso-input',
+  component: 'lukso-input',
+  argTypes: {
+    value: {
+      control: { type: 'text' },
+    },
+    name: {
+      control: { type: 'text' },
+    },
+    type: {
+      control: { type: 'text' },
+    },
+    placeholder: {
+      control: { type: 'text' },
+    },
+    label: {
+      control: { type: 'text' },
+    },
+    description: {
+      control: { type: 'text' },
+    },
+    error: {
+      control: { type: 'text' },
+    },
+    unit: {
+      control: { type: 'text' },
+    },
+    isFullWidth: {
+      control: { type: 'boolean' },
+    },
+    onKeyUp: {
+      description: 'Emitted on key up event.',
+      table: {
+        category: 'Events',
+      },
+    },
+  },
+  args: {
+    type: 'text',
+    value: '',
+    name: 'input',
+    label: '',
+    description: '',
+    error: '',
+    placeholder: '',
+    unit: '',
+    isFullWidth: false,
+  },
+  parameters: {
+    controls: {
+      exclude: [
+        'defaultStyles',
+        'errorStyles',
+        'unitStyles',
+        'is-full-width',
+        'hasHocus',
+      ],
+    },
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/NFCh20xAq3Jg2g8A0DNC9I/UI-Library?node-id=420%3A3617&t=JAexoWba0Re3ntDk-4',
+    },
+  },
+}
+
+const Template = ({
+  value,
+  name,
+  type,
+  placeholder,
+  label,
+  description,
+  error,
+  unit,
+  isFullWidth,
+}) =>
+  html`<lukso-input
+    value=${value}
+    name=${name}
+    type=${type}
+    placeholder=${placeholder}
+    label=${label}
+    description=${description}
+    error=${error}
+    unit=${unit}
+    ?is-full-width=${isFullWidth}
+    @on-key-up="handleKeyUp"
+  ></lukso-input>`
+
+/** Example of default input with `value`.  */
+export const DefaultInput = Template.bind({})
+DefaultInput.args = {
+  value: 'Text Input',
+}
+
+/** Example of input with `placeholder` text. */
+export const PlaceholderText = Template.bind({})
+PlaceholderText.args = {
+  placeholder: 'Placeholder Text',
+}
+
+/** Example of input with `label`. */
+export const Label = Template.bind({})
+Label.args = {
+  label: 'Title',
+}
+
+/** Example of input with `label` and `description`. */
+export const LabelAndDescription = Template.bind({})
+LabelAndDescription.args = {
+  label: 'Title',
+  description: 'Description',
+}
+
+/** Example of input with `error`. */
+export const Error = Template.bind({})
+Error.args = {
+  label: 'Title',
+  description: 'Description',
+  error: 'Error message',
+}
+
+/** Example of input with `unit`. */
+export const Unit = Template.bind({})
+Unit.args = {
+  unit: 'unit',
+}
+
+/** If you need input to take full width of the parent element add `is-full-width` property. */
+export const FullWidth = Template.bind({})
+FullWidth.args = {
+  isFullWidth: true,
+}

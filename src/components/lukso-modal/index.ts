530ba83d5ed3677a6eb24600816570010b40d08a
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-03-08 18:31:20 +0100

    feat: modal component (#79)

---
+++ src/components/lukso-modal/index.ts
@@ -0,0 +1,60 @@
+import { html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+
+import { TailwindElement } from '@/shared/tailwind-element'
+import { customClassMap } from '@/shared/directives'
+
+export type ModalSizes = 'small' | 'medium' | 'full'
+
+@customElement('lukso-modal')
+export class LuksoModal extends TailwindElement {
+  @property({ type: Boolean, attribute: 'is-open' })
+  isOpen = false
+
+  @property({ type: String })
+  size: ModalSizes = 'small'
+
+  private wrapperStyles = `opacity-0 fixed z-[1000] transition-opacity top-0 left-0 p-6 animate-fade-in animation-duration-200`
+  private openStyles = `flex opacity-100 visible items-center justify-center w-[100vw] h-[100vh]`
+  private overlayStyles = `bg-[rgba(36,53,66,0.8)] backdrop-blur-sm fixed top-0 left-0 w-[100vw] h-[100vh] z-[999]`
+  private dialogStyles = `bg-neutral-98 rounded-xl shadow-neutral-drop-shadow-3xl z-[1001]`
+
+  private close() {
+    this.isOpen = false
+  }
+
+  render() {
+    return html`
+      <div
+        data-testid="modal"
+        class=${customClassMap({
+          [this.wrapperStyles]: true,
+          [this.openStyles]: this.isOpen,
+          ['invisible']: !this.isOpen,
+        })}
+      >
+        <div
+          class=${customClassMap({
+            [this.overlayStyles]: true,
+          })}
+        ></div>
+        <div
+          class=${customClassMap({
+            [this.dialogStyles]: true,
+            ['w-[352px]']: this.size === 'small',
+            ['w-[849px]']: this.size === 'medium',
+            ['w-full']: this.size === 'full',
+          })}
+        >
+          <slot></slot>
+        </div>
+      </div>
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'lukso-modal': LuksoModal
+  }
+}
---
+++ src/components/lukso-modal/lukso-modal.stories.ts
@@ -0,0 +1,77 @@
+import { html } from 'lit-html'
+import { Meta } from '@storybook/web-components'
+import { useArgs } from '@storybook/client-api'
+
+import './index'
+import '../lukso-button'
+
+/**  Documentation and examples of `lukso-modal` component.  */
+const meta: Meta = {
+  title: 'Design System/Components/lukso-modal',
+  component: 'lukso-modal',
+  argTypes: {
+    isOpen: {
+      control: { type: 'boolean' },
+      description: 'Controls the visibility of the modal',
+    },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'full'],
+    },
+  },
+  args: {
+    isOpen: false,
+    size: 'small',
+  },
+  parameters: {
+    controls: {
+      exclude: [
+        'defaultStyles',
+        'wrapperStyles',
+        'openStyles',
+        'overlayStyles',
+        'dialogStyles',
+        'is-open',
+      ],
+    },
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/NFCh20xAq3Jg2g8A0DNC9I/UI-Library?node-id=1433%3A18445&t=nSFDgQdJOIWV2fQz-4',
+    },
+  },
+}
+
+export default meta
+
+const Template = ({ size }) => {
+  const [{ isOpen }, updateArgs] = useArgs()
+
+  const handleClose = () => {
+    updateArgs({ isOpen: false })
+  }
+
+  const handleOpen = () => {
+    updateArgs({ isOpen: true })
+  }
+
+  return html` <lukso-button variant="primary" @click=${handleOpen}
+      >Show modal</lukso-button
+    >
+    <lukso-modal ?is-open=${isOpen} size=${size}>
+      <div class="p-6">
+        <h1 class="heading-h1 pb-4">Lorem ipsum</h1>
+        <p class="paragraph-16-regular">
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+          posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel,
+          ullamcorper sit amet ligula. Proin eget tortor risus.
+        </p>
+        <p class="pt-6">
+          <lukso-button variant="primary" is-full-width @click=${handleClose}
+            >Close</lukso-button
+          >
+        </p>
+      </div>
+    </lukso-modal>`
+}
+
+export const DefaultModal = Template.bind({})

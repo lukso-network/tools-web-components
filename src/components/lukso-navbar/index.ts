f9b597ca26577aeeebd282b88c7b46afe1f7ac4f
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-04-21 18:47:42 +0200

    feat: navbar improvements (#131)

--- src/components/lukso-navbar/index.ts
+++ src/components/lukso-navbar/index.ts
@@ -1,5 +1,5 @@
 import { html } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import { customElement, property, state } from 'lit/decorators.js'
 
 import { TailwindElement } from '@/shared/tailwind-element'
 import { customClassMap } from '@/shared/directives'
@@ -18,7 +18,8 @@ export class LuksoNavbar extends TailwindElement {
   @property({ type: Boolean, attribute: 'is-transparent' })
   isTransparent = false
 
-  private defaultStyles = `bg-neutral-100 shadow-pink-drop-shadow h-78 flex`
+  @state()
+  private isMenuOpen = false
 
   private centerStyles = `justify-center`
 
@@ -34,16 +35,61 @@ export class LuksoNavbar extends TailwindElement {
     this.dispatchEvent(event)
   }
 
+  private handleMenuToggle() {
+    this.isMenuOpen = !this.isMenuOpen
+  }
+
+  desktopMenuTemplate() {
+    return html`<div
+      class="w-full items-center justify-end pr-10 no-underline hidden md:flex"
+    >
+      <slot name="desktop"></slot>
+    </div>`
+  }
+
+  mobileMenuTemplate() {
+    return html`<div
+      class="w-full items-center justify-end pr-6 flex md:hidden"
+    >
+      <div
+        class="flex items-center justify-center w-8 h-8 cursor-pointer"
+        @click=${this.handleMenuToggle}
+      >
+        <div
+          class="w-[18px] h-[2px] rounded-4 bg-neutral-20 transition-all
+        before:content-[''] before:absolute before:w-[18px] before:h-[2px] before:bg-neutral-20 before:rounded-4 before:transition-all before:-translate-y-[6px]
+        after:content-[''] after:absolute after:w-[18px] after:h-[2px] after:bg-neutral-20 after:rounded-4 after:transition-all after:translate-y-[6px]
+        ${customClassMap({
+            '!bg-transparent before:rotate-[45deg] before:!-translate-y-[0px] after:-rotate-[45deg] after:!translate-y-[0px]':
+              this.isMenuOpen,
+          })}
+      "
+        ></div>
+      </div>
+      <div
+        class="fixed top-[78px] left-0 w-full h-full bg-neutral-100 z-[1000] justify-center
+      items-center flex md:hidden
+    ${customClassMap({
+          'animate-fade-in animation-duration-150': this.isMenuOpen,
+          '!hidden': !this.isMenuOpen,
+        })}"
+        @click=${this.handleMenuToggle}
+      >
+        <slot name="mobile"></slot>
+      </div>
+    </div>`
+  }
+
   render() {
     return html`
       <nav
         data-testid="navbar"
-        class=${customClassMap({
-          [this.defaultStyles]: true,
+        class="bg-neutral-100 shadow-pink-drop-shadow h-78 flex
+        ${customClassMap({
           [this.centerStyles]: this.isCenter,
           [this.stickyStyles]: this.isSticky,
           [this.transparentStyles]: this.isTransparent,
-        })}
+        })}"
       >
         <div
           class="flex items-center px-7 h-full cursor-pointer sm:px-10"
@@ -57,7 +103,7 @@ export class LuksoNavbar extends TailwindElement {
             <span>${this.title}</span>
           </div>
         </div>
-        <slot></slot>
+        ${this.desktopMenuTemplate()} ${this.mobileMenuTemplate()}
       </nav>
     `
   }

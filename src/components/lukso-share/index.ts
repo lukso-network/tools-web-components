20eaf1a7576741745ef7d15e6052324c6d95b717
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-04-25 16:18:01 +0200

    Share links

---
+++ src/components/lukso-share/index.ts
@@ -0,0 +1,47 @@
+import { html } from 'lit'
+import { customElement } from 'lit/decorators.js'
+
+import { TailwindStyledElement } from '@/shared/tailwind-element'
+import style from './style.scss?inline'
+
+@customElement('lukso-share')
+export class LuksoShare extends TailwindStyledElement(style) {
+  private urls = {
+    twitter: 'https://twitter.com/lukso_io',
+    instagram: 'https://www.instagram.com/lukso/',
+    linkedin: 'https://www.linkedin.com/company/18289660/',
+    telegram: 'https://t.me/lukso',
+    discord: 'footer_discord_url',
+    reddit: 'https://www.reddit.com/r/lukso/',
+    medium: 'https://medium.com/lukso',
+    github: 'https://github.com/lukso-network',
+  }
+
+  linkTemplate(name: string, url: string) {
+    return html`<a
+      href=${url}
+      target="_blank"
+      class="transition-all duration-300 rounded-full hover:opacity-80 hover:shadow-button-hover-primary"
+      ><img src="/assets/images/social-${name}.svg" alt=""
+    /></a>`
+  }
+
+  render() {
+    const linkTemplates = []
+    for (const i of Object.keys(this.urls)) {
+      linkTemplates.push(this.linkTemplate(i, this.urls[i]))
+    }
+
+    return html`<div
+      class="grid gap-4 grid-cols-[repeat(4,max-content)] sm:grid-cols-[repeat(8,max-content)]"
+    >
+      ${linkTemplates}
+    </div>`
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'lukso-share': LuksoShare
+  }
+}

daa2652a6645717b9985353ab0fafeb31716ef52
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-04-26 10:15:20 +0200

    feat: share links (#138)

---
+++ src/components/lukso-share/index.ts
@@ -0,0 +1,55 @@
+import { html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+
+import { TailwindStyledElement } from '@/shared/tailwind-element'
+import style from './style.scss?inline'
+import { customClassMap } from '@/shared/directives'
+
+@customElement('lukso-share')
+export class LuksoShare extends TailwindStyledElement(style) {
+  @property({ type: String, attribute: 'custom-class' })
+  customClass = ''
+
+  private urls = {
+    twitter: 'https://twitter.com/lukso_io',
+    instagram: 'https://www.instagram.com/lukso/',
+    linkedin: 'https://www.linkedin.com/company/18289660/',
+    telegram: 'https://t.me/lukso',
+    discord: 'footer_discord_url',
+    reddit: 'https://www.reddit.com/r/lukso/',
+    medium: 'https://medium.com/lukso',
+    github: 'https://github.com/lukso-network',
+  }
+
+  linkTemplate(name: string, url: string) {
+    return html`<a
+      href=${url}
+      target="_blank"
+      class="transition-all duration-300 rounded-full hover:opacity-80 hover:shadow-button-hover-primary"
+      ><img src="/assets/images/social-${name}.svg" alt=""
+    /></a>`
+  }
+
+  render() {
+    const linkTemplates = []
+    for (const i of Object.keys(this.urls)) {
+      linkTemplates.push(this.linkTemplate(i, this.urls[i]))
+    }
+
+    return html`<div
+      class="grid gap-4 grid-cols-[repeat(4,max-content)] sm:grid-cols-[repeat(8,max-content)] ${customClassMap(
+        {
+          [this.customClass]: !!this.customClass,
+        }
+      )}"
+    >
+      ${linkTemplates}
+    </div>`
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'lukso-share': LuksoShare
+  }
+}
---
+++ src/components/lukso-share/lukso-share.stories.ts
@@ -0,0 +1,41 @@
+import { html } from 'lit-html'
+import { Meta } from '@storybook/web-components'
+
+import './index'
+
+/**  Documentation and examples of `lukso-share` component. */
+const meta: Meta = {
+  title: 'Design System/Components/lukso-share',
+  component: 'lukso-share',
+  argTypes: {
+    customClass: {
+      name: 'custom-class',
+      control: { type: 'text' },
+    },
+    'custom-class': {
+      name: 'customClass',
+    },
+  },
+  args: {
+    customClass: '',
+  },
+  parameters: {
+    controls: {
+      exclude: ['linkTemplate', 'urls', 'customClass'],
+    },
+  },
+}
+
+export default meta
+
+const Template = ({ customClass }) =>
+  html`<lukso-share custom-class=${customClass}></lukso-share>`
+
+/** Component render responsive list of links, use `custom-class` property to change layout of the elements. */
+export const Default = Template.bind({})
+Default.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/NFCh20xAq3Jg2g8A0DNC9I/UI-Library?node-id=1879-22788&t=mhehL8oWD5WMiaZM-4',
+  },
+}

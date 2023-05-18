b78e8f92db56fde43f8f05edaa2c30f4ea7725f1
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-05-18 12:14:05 +0200

    feat: switch icon (#160)

---
+++ src/components/lukso-icon/icons/switched.ts
@@ -0,0 +1,47 @@
+import { html } from 'lit'
+import { styleMap } from 'lit/directives/style-map.js'
+
+import { IconOptions } from '@/components/lukso-icon/index.js'
+
+export const switched = (options: IconOptions) => {
+  return html`<svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    style=${styleMap({
+      width: `${options.width}px`,
+      height: `${options.height}px`,
+    })}
+  >
+    <path
+      d="M7.82847 15.1716C6.53981 16.4603 5 18 5 18L7.82843 20.8284"
+      stroke="var(--${options.color})"
+      stroke-width="${options.strokeWidth}"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M16.1715 8.82838C17.4602 7.53972 19 6 19 6L16.1716 3.17157"
+      stroke="var(--${options.color})"
+      stroke-width="${options.strokeWidth}"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M12.1176 12H16C17.6569 12 19 13.3431 19 15V15C19 16.6569 17.6569 18 16 18H6"
+      stroke="var(--${options.color})"
+      stroke-width="${options.strokeWidth}"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M18 6H9C7.34315 6 6 7.34315 6 9V9C6 10.6569 7.34315 12 9 12H13"
+      stroke="var(--${options.color})"
+      stroke-width="${options.strokeWidth}"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg> `
+}

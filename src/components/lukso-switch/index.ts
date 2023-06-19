e5bb392167dc6d891f6f2b5f835a01a2b8ba2e79
Andy Ogaga <44970013+andyogaga@users.noreply.github.com>
2023-06-19 11:06:08 +0100

    feat: adds switch (#172)

---
+++ src/components/lukso-switch/index.ts
@@ -0,0 +1,79 @@
+import { html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+import { styleMap } from 'lit/directives/style-map.js'
+
+import { TailwindElement } from '@/shared/tailwind-element'
+import { customClassMap } from '@/shared/directives'
+
+@customElement('lukso-switch')
+export class LuksoSwitch extends TailwindElement {
+  @property({ type: String })
+  color = 'green-54'
+
+  @property({ type: Boolean, attribute: 'is-checked' })
+  private isChecked = false
+
+  @property({
+    type: Boolean,
+    attribute: 'is-disabled',
+  })
+  private isDisabled = false
+
+  private handleChange(event: Event) {
+    this.isChecked = !this.isChecked
+    const target = event.target as HTMLInputElement
+    const blurEvent = new CustomEvent('on-change', {
+      detail: {
+        value: target.value,
+        event,
+      },
+      bubbles: false,
+      composed: true,
+    })
+    this.dispatchEvent(blurEvent)
+  }
+
+  private defaultLabelStyles = `transition duration-300 ease-in block h-6 overflow-hidden rounded-full relative inline-block w-10`
+
+  private defaultInputStyles = `absolute block w-6 h-6 rounded-full bg-white border-2 appearance-none transition duration-300 ease-in`
+
+  render() {
+    return html`
+      <label
+        for="switch"
+        class=${customClassMap({
+          [this.defaultLabelStyles]: true,
+        })}
+        style=${styleMap({
+          backgroundColor: `var(--${
+            this.isChecked ? this.color : 'neutral-90'
+          })`,
+          opacity: this.isDisabled ? 0.7 : 1,
+          cursor: this.isDisabled ? 'not-allowed' : 'pointer',
+        })}
+      >
+        <input
+          type="checkbox"
+          id="switch"
+          ?checked=${this.isChecked}
+          ?disabled=${this.isDisabled}
+          class=${customClassMap({
+            [this.defaultInputStyles]: true,
+            ['translate-x-4']: this.isChecked,
+          })}
+          style=${styleMap({
+            borderColor: `var(--${this.isChecked ? this.color : 'neutral-90'})`,
+            cursor: this.isDisabled ? 'not-allowed' : 'pointer',
+          })}
+          @change=${this.handleChange}
+        />
+      </label>
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'lukso-switch': LuksoSwitch
+  }
+}
---
+++ src/components/lukso-switch/lukso-switch.stories.ts
@@ -0,0 +1,88 @@
+import { html } from 'lit-html'
+import { Meta } from '@storybook/web-components'
+
+import './index'
+
+/**  Documentation and examples of `lukso-switch` component.  */
+const meta: Meta = {
+  title: 'Design System/Forms/lukso-switch',
+  component: 'lukso-switch',
+  argTypes: {
+    color: {
+      control: {
+        type: 'text',
+      },
+    },
+    isDisabled: {
+      control: {
+        type: 'boolean',
+      },
+      name: 'is-disabled',
+    },
+    isChecked: {
+      name: 'is-checked',
+      control: {
+        type: 'boolean',
+      },
+    },
+    'is-checked': {
+      name: 'isChecked',
+    },
+    'is-disabled': {
+      name: 'isDisabled',
+    },
+    onChange: {
+      name: 'on-change',
+      description: 'Emitted when checkbox is clicked',
+      table: {
+        category: 'Events',
+      },
+    },
+  },
+  args: {
+    color: 'green-54',
+    isDisabled: false,
+    isChecked: false,
+  },
+  parameters: {
+    controls: {
+      exclude: [
+        'defaultLabelStyles',
+        'defaultInputStyles',
+        'checked',
+        'isDisabled',
+        'isChecked',
+      ],
+    },
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/NFCh20xAq3Jg2g8A0DNC9I/UI-Library?type=design&node-id=376-3984&t=20P0yRhnEEulAelu-0',
+    },
+  },
+}
+
+export default meta
+
+const Template = ({ color, onChange, isDisabled, isChecked }) =>
+  html`<lukso-switch
+    @on-change=${onChange}
+    color=${color}
+    ?is-disabled=${isDisabled}
+    ?is-checked=${isChecked}
+  ></lukso-switch>`
+
+/** Example of default switch.  */
+export const DefaultInput = Template.bind({})
+DefaultInput.args = {
+  color: 'green-54',
+  isChecked: true,
+  isDisabled: false,
+}
+
+/** Example of disabled switch.  */
+export const DisabledInput = Template.bind({})
+DisabledInput.args = {
+  color: 'red-65',
+  isChecked: true,
+  isDisabled: true,
+}

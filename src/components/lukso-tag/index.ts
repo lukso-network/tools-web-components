11b75b671afa06988d6e1723d86bcbc60dce9832
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-08-01 13:35:34 +0200

    feat: extra small tag (#184)

--- src/components/lukso-tag/index.ts
+++ src/components/lukso-tag/index.ts
@@ -2,13 +2,14 @@ import { html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 import { styleMap } from 'lit/directives/style-map.js'
 
-import { TailwindElement } from '@/shared/tailwind-element'
+import { TailwindStyledElement } from '@/shared/tailwind-element'
 import { customClassMap } from '@/shared/directives'
+import style from './style.scss?inline'
 
-export type TagSizes = 'small' | 'large'
+export type TagSizes = 'x-small' | 'small' | 'large'
 
 @customElement('lukso-tag')
-export class LuksoTag extends TailwindElement {
+export class LuksoTag extends TailwindStyledElement(style) {
   @property({ type: String })
   size: TagSizes = 'small'
 
@@ -22,10 +23,15 @@ export class LuksoTag extends TailwindElement {
   textColor = ''
 
   private defaultStyles = `inline-flex items-center justify-center border border-neutral-20 text-neutral-20`
+  private extraSmallStyles = `font-inter text-8 font-500 leading-20 h-[20px]`
   private smallStyles = `paragraph-inter-12-medium h-[28px]`
   private largeStyles = `paragraph-inter-14-medium h-[34px]`
 
   private padding() {
+    if (this.size === 'x-small' && !this.isRounded) {
+      return 'px-1'
+    }
+
     if (this.size === 'small' && this.isRounded) {
       return 'px-3'
     }
@@ -45,7 +51,9 @@ export class LuksoTag extends TailwindElement {
           [this.defaultStyles]: true,
           [this.padding()]: true,
           ['rounded-[56px]']: this.isRounded,
-          ['rounded-8']: !this.isRounded,
+          ['rounded-8']: !this.isRounded && this.size !== 'x-small',
+          ['rounded-4']: !this.isRounded && this.size === 'x-small',
+          [this.extraSmallStyles]: this.size === 'x-small',
           [this.smallStyles]: this.size === 'small',
           [this.largeStyles]: this.size === 'large',
         })}

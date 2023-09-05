4d16ebbaf98df1b6702898095b59ce1ac5455520
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-09-05 15:01:50 +0200

    Footer component

---
+++ src/components/lukso-footer/index.ts
@@ -0,0 +1,39 @@
+import { html } from 'lit'
+import { customElement } from 'lit/decorators.js'
+
+import { TailwindElement } from '@/shared/tailwind-element'
+import '@/components/lukso-share'
+
+@customElement('lukso-footer')
+export class LuksoFooter extends TailwindElement {
+  render() {
+    return html`
+      <footer class="bg-neutral-100">
+        <div
+          class="p-10 grid gap-12 items-center lg:grid-cols-[auto,max-content]"
+        >
+          <slot name="top"></slot>
+          <div className="flex lg:justify-end">
+            <lukso-share></lukso-share>
+          </div>
+        </div>
+        <div
+          class="grid sm:grid-cols-[max-content,auto] px-10 py-6 bg-neutral-95 gap-8"
+        >
+          <slot name="bottom"></slot>
+          <div class="flex sm:justify-end">
+            <a href="https://lukso.network/" target="_blank">
+              <img src="/assets/images/powered-by.svg" alt="Powered by LUKSO" />
+            </a>
+          </div>
+        </div>
+      </footer>
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'lukso-footer': LuksoFooter
+  }
+}

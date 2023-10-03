6e18dcb71423832dccd606b48fe415646404ea0d
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-10-03 09:32:04 +0200

    Update `lukso-share` component

--- src/components/lukso-share/index.ts
+++ src/components/lukso-share/index.ts
@@ -3,14 +3,41 @@ import { customElement, property } from 'lit/decorators.js'
 
 import { TailwindStyledElement } from '@/shared/tailwind-element'
 import style from './style.scss?inline'
-import { customClassMap } from '@/shared/directives'
+
+export type Provider =
+  | 'twitter'
+  | 'instagram'
+  | 'linkedin'
+  | 'telegram'
+  | 'discord'
+  | 'reddit'
+  | 'medium'
+  | 'github'
+
+export type Url = `https://${string}`
+
+export type ProviderLinks = {
+  [key in Provider]: Url
+}
 
 @customElement('lukso-share')
 export class LuksoShare extends TailwindStyledElement(style) {
-  @property({ type: String, attribute: 'custom-class' })
-  customClass = ''
+  @property({ type: String, attribute: 'custom-style' })
+  customStyle = ''
 
-  private urls = {
+  @property({ type: Array })
+  providers: Provider[] = [
+    'twitter',
+    'instagram',
+    'linkedin',
+    'telegram',
+    'discord',
+    'reddit',
+    'medium',
+    'github',
+  ]
+
+  private urls: ProviderLinks = {
     twitter: 'https://twitter.com/lukso_io',
     instagram: 'https://www.instagram.com/lukso',
     linkedin: 'https://linkedin.com/company/lukso',
@@ -21,27 +48,27 @@ export class LuksoShare extends TailwindStyledElement(style) {
     github: 'https://github.com/lukso-network',
   }
 
-  linkTemplate(name: string, url: string) {
+  linkTemplate(name: Provider, url: Url) {
     return html`<a
       href=${url}
       target="_blank"
-      class="transition-all duration-300 rounded-full hover:opacity-80 hover:shadow-button-hover-primary"
+      class="transition-all duration-300 rounded-full opacity-50 hover:opacity-80 hover:shadow-button-hover-primary h-7 w-7"
       ><img src="/assets/images/social-${name}.svg" alt=""
     /></a>`
   }
 
   render() {
     const linkTemplates = []
-    for (const i of Object.keys(this.urls)) {
-      linkTemplates.push(this.linkTemplate(i, this.urls[i]))
+
+    for (const provider of Object.keys(this.urls) as Provider[]) {
+      if (this.providers.includes(provider)) {
+        linkTemplates.push(this.linkTemplate(provider, this.urls[provider]))
+      }
     }
 
     return html`<div
-      class="grid gap-4 grid-cols-[repeat(4,max-content)] sm:grid-cols-[repeat(8,max-content)] ${customClassMap(
-        {
-          [this.customClass]: !!this.customClass,
-        }
-      )}"
+      class="grid gap-2 grid-cols-[repeat(8,max-content)] sm:gap-4"
+      style=${this.customStyle}
     >
       ${linkTemplates}
     </div>`

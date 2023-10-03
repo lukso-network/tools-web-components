539ae3e64e88b1aa1690308467c60218c03ec943
Dominik Zborowski <dominik.zborowski@gmail.com>
2023-10-03 09:32:31 +0200

    Update `lukso-footer` component

--- src/components/lukso-footer/lukso-footer.stories.ts
+++ src/components/lukso-footer/lukso-footer.stories.ts
@@ -25,31 +25,45 @@ export default meta
 const Template = ({}) =>
   html`<lukso-footer>
     <div
-      slot="top"
-      class="grid gap-6 items-center sm:gap-10
-      sm:grid-cols-[repeat(2,max-content)]"
+      slot="links"
+      class="grid gap-4 items-center grid-cols-1 grid-rows-2
+      xl:grid-cols-[repeat(3,max-content)] xl:gap-10 xl:grid-rows-1"
     >
-      <a
-        href=""
-        class="nav-apax-12-medium-uppercase text-purple-41 hover:underline hover:text-purple-31"
-      >
-        FAQs
-      </a>
-      <a
-        href=""
-        class="nav-apax-12-medium-uppercase text-purple-41 hover:underline hover:text-purple-31"
-      >
-        NEED HELP?
-      </a>
-    </div>
-
-    <div slot="bottom" class="gap-8 flex items-center">
-      <a href="" class="paragraph-inter-12-medium hover:underline">
-        Privacy Policy
-      </a>
-      <a href="" class="paragraph-inter-12-medium hover:underline">
-        Terms & Conditions
-      </a>
+      <div class="flex gap-10">
+        <a
+          href=""
+          class="nav-apax-12-medium-uppercase text-purple-41 hover:underline hover:text-purple-31"
+        >
+          FAQs
+        </a>
+        <a
+          href=""
+          class="nav-apax-12-medium-uppercase text-purple-41 hover:underline hover:text-purple-31"
+        >
+          NEED HELP?
+        </a>
+        <a
+          href=""
+          class="nav-apax-12-medium-uppercase text-purple-41 hover:underline hover:text-purple-31"
+        >
+          GITHUB
+        </a>
+      </div>
+      <div class="hidden w-0.5 h-4 bg-purple-63 xl:block"></div>
+      <div class="flex gap-10">
+        <a
+          href=""
+          class="nav-apax-12-medium-uppercase text-purple-41/70 hover:underline hover:text-purple-31/70"
+        >
+          PRIVACY POLICY
+        </a>
+        <a
+          href=""
+          class="nav-apax-12-medium-uppercase text-purple-41/70 hover:underline hover:text-purple-31/70"
+        >
+          TERMs
+        </a>
+      </div>
     </div>
   </lukso-footer>`
 

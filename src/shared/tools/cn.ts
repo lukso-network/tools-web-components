4ddafcd385c57095e6acda418c2ccf7a2ca8afe8
Pedro Pereira <cyberafael@msn.com>
2023-11-28 12:26:53 +0000

    fix(DEV-9202): fixed wrong twMerge config. Fixed fonts inter extra bold

--- src/shared/tools/cn.ts
+++ src/shared/tools/cn.ts
@@ -13,20 +13,24 @@ import {
 } from './tailwind-config'
 
 const customTwMerge = extendTailwindMerge({
-  classGroups: {
-    shadow: Object.keys(boxShadows).map(s => `shadow-${s}`),
-    'font-size': Object.keys(fontSizes).map(s => `text-${s}`),
-    'font-weight': Object.keys(fontWeights).map(s => `font-${s}`),
-    'line-height': Object.keys(lineHeights).map(s => `leading-${s}`),
-    scale: Object.keys(scales).map(s => `scale-${s}`),
-    height: Object.keys(heights).map(s => `h-${s}`),
-    borderRadius: Object.keys(borderRadius).map(s => `rounded-${s}`),
-    colors: Object.entries(colorPalette).reduce((acc, currentVal) => {
-      const colors = Object.keys(currentVal[1]).map(
-        c => `text-${currentVal[0]}-${c}`
-      )
-      return [...acc, ...colors]
-    }, []),
+  extend: {
+    theme: {
+      colors: Object.entries(colorPalette).reduce((acc, currentVal) => {
+        const colors = Object.keys(currentVal[1]).map(
+          c => `text-${currentVal[0]}-${c}`
+        )
+        return [...acc, ...colors]
+      }, []),
+      borderRadius: Object.keys(borderRadius).map(s => `rounded-${s}`),
+    },
+    classGroups: {
+      shadow: [{ shadow: Object.keys(boxShadows).map(s => `shadow-${s}`) }],
+      'font-size': Object.keys(fontSizes).map(s => `text-${s}`),
+      'font-weight': Object.keys(fontWeights).map(s => `font-${s}`),
+      scale: Object.keys(scales).map(s => `scale-${s}`),
+      h: Object.keys(heights).map(s => `h-${s}`),
+      leading: Object.keys(lineHeights).map(s => `leading-${s}`),
+    },
   },
 })
 

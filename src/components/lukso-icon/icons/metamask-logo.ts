4340ce48903802590bdf4246da892eb8c149ae91
Pedro Pereira <cyberafael@msn.com>
2024-02-01 23:25:11 +0000

    feat: add icon metamask-logo (#305)

---
+++ src/components/lukso-icon/icons/metamask-logo.ts
@@ -0,0 +1,292 @@
+import { html } from 'lit'
+import { styleMap } from 'lit/directives/style-map.js'
+
+import { IconOptions } from '@/components/lukso-icon/index.js'
+
+export const metamaskLogo = (options: IconOptions) => {
+  return html`<svg
+    width="28"
+    height="26"
+    viewBox="0 0 28 26"
+    fill="none"
+    style=${styleMap({
+      width: `${options.width}px`,
+      height: `${options.height}px`,
+    })}
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g id="metamask">
+      <path
+        id="Vector"
+        d="M25.5986 1L15.4971 8.50254L17.3651 4.07614L25.5986 1Z"
+        fill="#E2761B"
+        stroke="#E2761B"
+        stroke-width="1.25143"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <g id="Group">
+        <path
+          id="Vector_2"
+          d="M2.2793 1L12.2996 8.5736L10.5229 4.07614L2.2793 1Z"
+          fill="#E4761B"
+          stroke="#E4761B"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_3"
+          d="M21.9648 18.3909L19.2744 22.5127L25.0308 24.0965L26.6856 18.4822L21.9648 18.3909Z"
+          fill="#E4761B"
+          stroke="#E4761B"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_4"
+          d="M1.21289 18.4822L2.85756 24.0965L8.6139 22.5127L5.92355 18.3909L1.21289 18.4822Z"
+          fill="#E4761B"
+          stroke="#E4761B"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_5"
+          d="M8.28961 11.4265L6.68555 13.8529L12.4013 14.1067L12.1982 7.9646L8.28961 11.4265Z"
+          fill="#E4761B"
+          stroke="#E4761B"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_6"
+          d="M19.5884 11.4263L15.629 7.89331L15.4971 14.1065L21.2027 13.8527L19.5884 11.4263Z"
+          fill="#E4761B"
+          stroke="#E4761B"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_7"
+          d="M8.61426 22.5128L12.0457 20.8377L9.08126 18.5229L8.61426 22.5128Z"
+          fill="#E4761B"
+          stroke="#E4761B"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_8"
+          d="M15.832 20.8377L19.2737 22.5128L18.7965 18.5229L15.832 20.8377Z"
+          fill="#E4761B"
+          stroke="#E4761B"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <g id="Group_2">
+        <path
+          id="Vector_9"
+          d="M19.2746 22.5128L15.833 20.8376L16.1071 23.0813L16.0767 24.0255L19.2746 22.5128Z"
+          fill="#D7C1B3"
+          stroke="#D7C1B3"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_10"
+          d="M8.61426 22.5128L11.8122 24.0255L11.7919 23.0813L12.0457 20.8376L8.61426 22.5128Z"
+          fill="#D7C1B3"
+          stroke="#D7C1B3"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <path
+        id="Vector_11"
+        d="M11.8629 17.0407L9 16.198L11.0203 15.2742L11.8629 17.0407Z"
+        fill="#233447"
+        stroke="#233447"
+        stroke-width="1.25143"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        id="Vector_12"
+        d="M16.0156 17.0407L16.8583 15.2742L18.8887 16.198L16.0156 17.0407Z"
+        fill="#233447"
+        stroke="#233447"
+        stroke-width="1.25143"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <g id="Group_3">
+        <path
+          id="Vector_13"
+          d="M8.61418 22.5127L9.10149 18.3909L5.92383 18.4822L8.61418 22.5127Z"
+          fill="#CD6116"
+          stroke="#CD6116"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_14"
+          d="M18.7871 18.3909L19.2744 22.5127L21.9648 18.4822L18.7871 18.3909Z"
+          fill="#CD6116"
+          stroke="#CD6116"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_15"
+          d="M21.2027 13.8528L15.4971 14.1066L16.025 17.0406L16.8676 15.2741L18.8981 16.198L21.2027 13.8528Z"
+          fill="#CD6116"
+          stroke="#CD6116"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_16"
+          d="M9.00027 16.198L11.0307 15.2741L11.8632 17.0406L12.4013 14.1066L6.68555 13.8528L9.00027 16.198Z"
+          fill="#CD6116"
+          stroke="#CD6116"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <g id="Group_4">
+        <path
+          id="Vector_17"
+          d="M6.68555 13.8528L9.08149 18.5228L9.00027 16.198L6.68555 13.8528Z"
+          fill="#E4751F"
+          stroke="#E4751F"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_18"
+          d="M18.8984 16.198L18.7969 18.5228L21.203 13.8528L18.8984 16.198Z"
+          fill="#E4751F"
+          stroke="#E4751F"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_19"
+          d="M12.4014 14.1067L11.8633 17.0407L12.5333 20.5026L12.6856 15.9443L12.4014 14.1067Z"
+          fill="#E4751F"
+          stroke="#E4751F"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_20"
+          d="M15.4977 14.1067L15.2236 15.9341L15.3455 20.5026L16.0257 17.0407L15.4977 14.1067Z"
+          fill="#E4751F"
+          stroke="#E4751F"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <path
+        id="Vector_21"
+        d="M16.0249 17.0406L15.3447 20.5026L15.832 20.8376L18.7965 18.5229L18.898 16.198L16.0249 17.0406Z"
+        fill="#F6851B"
+        stroke="#F6851B"
+        stroke-width="1.25143"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        id="Vector_22"
+        d="M9 16.198L9.08122 18.5229L12.0457 20.8376L12.533 20.5026L11.8629 17.0406L9 16.198Z"
+        fill="#F6851B"
+        stroke="#F6851B"
+        stroke-width="1.25143"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        id="Vector_23"
+        d="M16.0762 24.0254L16.1066 23.0812L15.8528 22.8579H12.0254L11.7919 23.0812L11.8122 24.0254L8.61426 22.5127L9.73101 23.4264L11.995 25H15.8833L18.1574 23.4264L19.2742 22.5127L16.0762 24.0254Z"
+        fill="#C0AD9E"
+        stroke="#C0AD9E"
+        stroke-width="1.25143"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        id="Vector_24"
+        d="M15.8326 20.8375L15.3453 20.5024H12.5331L12.0458 20.8375L11.792 23.0811L12.0255 22.8578H15.8529L16.1067 23.0811L15.8326 20.8375Z"
+        fill="#161616"
+        stroke="#161616"
+        stroke-width="1.25143"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <g id="Group_5">
+        <path
+          id="Vector_25"
+          d="M26.0259 8.98985L26.8888 4.84772L25.5995 1L15.833 8.24873L19.5894 11.4264L24.899 12.9797L26.0767 11.6091L25.569 11.2437L26.3812 10.5025L25.7518 10.0152L26.564 9.39594L26.0259 8.98985Z"
+          fill="#763D16"
+          stroke="#763D16"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_26"
+          d="M1 4.84772L1.86294 8.98985L1.31472 9.39594L2.1269 10.0152L1.50761 10.5025L2.3198 11.2437L1.81218 11.6091L2.9797 12.9797L8.28934 11.4264L12.0457 8.24873L2.27919 1L1 4.84772Z"
+          fill="#763D16"
+          stroke="#763D16"
+          stroke-width="1.25143"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <path
+        id="Vector_27"
+        d="M24.8984 12.9798L19.5888 11.4265L21.203 13.8529L18.7969 18.523L21.9644 18.4824H26.6852L24.8984 12.9798Z"
+        fill="#F6851B"
+        stroke="#F6851B"
+        stroke-width="1.25143"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        id="Vector_28"
+        d="M8.28903 11.4265L2.97939 12.9798L1.21289 18.4824H5.92355L9.08091 18.523L6.68497 13.8529L8.28903 11.4265Z"
+        fill="#F6851B"
+        stroke="#F6851B"
+        stroke-width="1.25143"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        id="Vector_29"
+        d="M15.4971 14.1066L15.8321 8.24876L17.3753 4.07617H10.5225L12.0453 8.24876L12.4006 14.1066L12.5225 15.9543L12.5326 20.5026H15.3448L15.3651 15.9543L15.4971 14.1066Z"
+        fill="#F6851B"
+        stroke="#F6851B"
+        stroke-width="1.25143"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+  </svg>`
+}

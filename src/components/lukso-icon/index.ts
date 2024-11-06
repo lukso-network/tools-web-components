f04d6a6b91e21629e0f6326b57295cb1f459396c
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-11-07 00:37:54 +0100

    Gallery icon

--- src/components/lukso-icon/index.ts
+++ src/components/lukso-icon/index.ts
@@ -166,6 +166,7 @@ import { emptyOutline } from './icons/empty-outline'
 import { profileRemove } from './icons/profile-remove'
 import { logoYoutubeMono } from './icons/logo-youtube-mono'
 import { addGrid } from './icons/add-grid'
+import { gallery } from './icons/gallery'
 
 export type IconOptions = {
   width: number
@@ -347,6 +348,7 @@ const iconMap = {
   'profile-remove': profileRemove,
   'logo-youtube-mono': logoYoutubeMono,
   'add-grid': addGrid,
+  gallery,
 }
 
 @customElement('lukso-icon')

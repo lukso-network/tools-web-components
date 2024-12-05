3e7d4ff365fddf81de64ee7deb4b5cfddbfb1fe7
Dominik Zborowski <dominik.zborowski@gmail.com>
2024-12-05 11:36:50 +0100

    feat: resize icons (#447)

---
+++ src/components/lukso-icon/icons/minimize-vertical.ts
@@ -0,0 +1,47 @@
+import { html } from 'lit'
+import { styleMap } from 'lit/directives/style-map.js'
+
+import type { IconOptions } from '@/components/lukso-icon/index.js'
+
+export const minimizeVertical = (options: IconOptions) => {
+  return html`<svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    style=${styleMap({
+      width: `${options.width}px`,
+      height: `${options.height}px`,
+    })}
+  >
+    <path
+      d="M12.3238 3.77781V8.62654"
+      stroke="var(--${options.color})"
+      stroke-width="1.3125"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M9.49183 7.01028L12.3203 9.83871L15.1487 7.01028"
+      stroke="var(--${options.color})"
+      stroke-width="1.3125"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M12.3313 16.0754V20.9241"
+      stroke="var(--${options.color})"
+      stroke-width="1.3125"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M15.1484 17.6917L12.32 14.8633L9.49158 17.6917"
+      stroke="var(--${options.color})"
+      stroke-width="1.3125"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg> `
+}

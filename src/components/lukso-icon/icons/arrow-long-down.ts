4eca1e8b18ca3a0e222875fb2e8646a6d38e87c3
Dominik Zborowski <dominik.zborowski@gmail.com>
2025-02-03 16:09:56 +0100

    feat: Add arrow-long-down icon (#494)

---
+++ src/components/lukso-icon/icons/arrow-long-down.ts
@@ -0,0 +1,33 @@
+import { html } from 'lit'
+import { styleMap } from 'lit/directives/style-map.js'
+
+import type { IconOptions } from '@/components/lukso-icon/index.js'
+
+export const arrowLongDown = (options: IconOptions) => {
+  return html`<svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    style=${styleMap({
+      width: `${options.width}px`,
+      height: `${options.height}px`,
+    })}
+  >
+    <path
+      d="M8.99999 16.1715C10.2887 17.4602 11.8284 19 11.8284 19L14.6568 16.1716"
+      stroke="var(--${options.color})"
+      stroke-width="${options.strokeWidth}"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M11.8284 6.00003L11.8284 18"
+      stroke="var(--${options.color})"
+      stroke-width="${options.strokeWidth}"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg> `
+}

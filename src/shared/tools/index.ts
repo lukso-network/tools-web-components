ae74d983f4ce88c791dc071c1e873cc783f74fb4
Dominik Zborowski <dominik.zborowski@gmail.com>
2025-02-14 10:54:14 +0100

    Fix exports

--- src/shared/tools/index.ts
+++ src/shared/tools/index.ts
@@ -1,7 +1,6 @@
-export * from './tailwind-config'
-export * from './copy-assets'
+// include only browser imports
 export * from './hsl-color-map'
 export * from './slice-address'
-export * from '../assets'
 export * from './cn'
 export * from './detect-social-media'
+export * from './debounceFunction'

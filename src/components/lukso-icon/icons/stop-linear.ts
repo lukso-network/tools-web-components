c50a6a4f42ee5b5827b1f8a9ac12db8681f7fa83
Dominik Zborowski <dominik.zborowski@gmail.com>
2025-05-12 12:23:48 +0200

    Add new icons

---
+++ src/components/lukso-icon/icons/stop-linear.ts
@@ -0,0 +1,26 @@
+import { html } from 'lit'
+import { styleMap } from 'lit/directives/style-map.js'
+
+import type { IconOptions } from '@/components/lukso-icon/index.js'
+
+export const stopLinear = (options: IconOptions) => {
+  return html`<svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    style=${styleMap({
+      width: `${options.width}px`,
+      height: `${options.height}px`,
+    })}
+  >
+    <path
+      d="M9.3 21H14.7C19.2 21 21 19.2 21 14.7V9.3C21 4.8 19.2 3 14.7 3H9.3C4.8 3 3 4.8 3 9.3V14.7C3 19.2 4.8 21 9.3 21Z"
+      stroke="var(--${options.color})"
+      stroke-width="${options.strokeWidth}"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg> `
+}

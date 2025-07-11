bf4a87c38bb3eb54460238799ff6b0cc71b7aef1
Dominik Zborowski <dominik.zborowski@gmail.com>
2025-07-11 14:14:58 +0200

    Fix group label alignment

--- src/components/lukso-search/index.ts
+++ src/components/lukso-search/index.ts
@@ -260,7 +260,7 @@ export class LuksoSearch extends TailwindStyledElement(style) {
           }
           resultTemplates.push(
             html`<div
-              class="py-1 my-1 text-neutral-35 paragraph-inter-12-medium tracking-wider border-b border-b-neutral-95"
+              class="py-1 my-1 text-neutral-35 paragraph-inter-12-medium tracking-wider border-b border-b-neutral-95 text-left"
             >
               ${headerText}
             </div>`

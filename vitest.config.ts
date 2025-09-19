c852aad98e8228fea77fc306885a22b24e315c55
Dominik Zborowski <dominik.zborowski@gmail.com>
2025-09-19 12:34:54 +0200

    Test setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vitest/config'
+import path from 'node:path'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@/components': path.resolve(__dirname, './src/components'),
+      '@/shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
   test: {
     exclude: ['**/node_modules/**'],
     globals: true,
+    environment: 'jsdom',
   },
 })

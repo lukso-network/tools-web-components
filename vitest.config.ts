978502fb629d1bee347c5bb2c770566d8e59c2f7
Dominik Zborowski <dominik.zborowski@gmail.com>
2025-09-23 17:46:11 +0200

    Fix unit test config + add coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,22 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    include: ['src/shared/tools/**/*.spec.ts'],
     exclude: ['**/node_modules/**'],
     globals: true,
+    coverage: {
+      provider: 'v8',
+      include: ['src/shared/tools/**'],
+      exclude: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/*.d.ts',
+        '**/*.test.*',
+        '**/*.spec.*',
+        '**/coverage/**',
+      ],
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: './coverage',
+    },
   },
 })

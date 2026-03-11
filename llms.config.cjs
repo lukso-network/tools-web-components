2405f0d7911e9d13a2c0ddb93b49b16d52bad42f
Dominik Zborowski <dominik.zborowski@gmail.com>
2026-03-11 17:17:10 +0100

    feat: Add llm documentation to web-components (#828)

---
+++ llms.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  distPath: 'storybook-static',
+  summaryBaseUrl: 'https://tools-web-components.pages.dev',
+  summaryTitle: 'LUKSO Web Components',
+  summaryDescription:
+    'Documentation for @lukso/web-components — Lit + Tailwind CSS web components library.',
+}
